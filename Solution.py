af439b5f957ed4d6c8d5d5ab1229baa243e78752
bvogels <benjamin.vogels@gmx.de>
2022-05-19 16:01:17 +0200

    Working on the Solution function

--- Solution.py
+++ Solution.py
@@ -3,7 +3,7 @@ class Solution:
         self.grid = grid.grid
 
     default_goal_state = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-    costs = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
+    costs = {0: 0, 1: 9, 2: 9, 3: 9, 4: 9, 5: 9, 6: 9, 7: 9, 8: 9}
 
     def get_tile(self, tile, search_grid):
         null_coords = []
@@ -17,15 +17,15 @@ class Solution:
         if self.grid == self.default_goal_state:
             return True
         self.calculate_cost()
-        #for row in self.grid:
-        #    for col in self.grid:
-        #        if self.grid[row][col] != self.default_goal_state[row][col]:
-        #            print(0)
+        fringe = {key: value for (key, value) in self.costs.items() if value > 0}
+        to_move = min(fringe, key = fringe.get)
+        print("x")
+
 
     def calculate_cost(self):
         null_position = self.get_tile(0, self.grid)
         if null_position[0] == 0:
-            if null_position[1] == 0: # Calculate costs from 0 position than add 1
+            if null_position[1] == 0:  # Calculate costs from 0 position than add 1
                 to_move_coords = [[0, 1], [1, 0]]
                 self.calculate_distance(to_move_coords, null_position)
             elif null_position[1] == 1:
@@ -59,7 +59,8 @@ class Solution:
         for coords in to_move_coords:
             to_move = self.grid[coords[0]][coords[1]]
             target_position = self.get_tile(to_move, self.default_goal_state)
-            self.costs[to_move] = abs(null_position[1] - target_position[1] + target_position[0]) + 1
+            self.costs[to_move] = abs(null_position[1] - target_position[1]) + abs(
+                null_position[0] - target_position[0]) + 1
 
     def right(self, row, col):
         self.grid[row][col + 1] = self.grid[row][col]
