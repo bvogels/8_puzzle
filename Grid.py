c163d6577ff291054246375c2019871a88778f74
bvogels <benjamin.vogels@gmx.de>
2022-05-19 20:46:50 +0200

    Some improvements. The challenge is now how to handle it if the 0 is stuck in between correctly placed tiles.

---
+++ Grid.py
@@ -0,0 +1,83 @@
+import random
+import matplotlib.pyplot as plt
+
+
+class Grid:
+    grid = [[], [], []]
+
+    ###
+    ###### Create a 3x3 puzzle with the numbers 0 to 8
+    #########
+    ######
+    ###
+
+    def create_grid(self):
+        r, e = 0, 0
+        numbers = [n for n in range(0, 9)]
+        random.shuffle(numbers)
+        while r < 3:
+            c = 0
+            while c < 3:
+                self.grid[r].append(numbers[e])
+                c += 1
+                e += 1
+            r += 1
+        print(self.grid)
+
+    ###
+    ###### Check if the puzzle is even solvable.
+    #########
+    ######
+    ###
+
+    def check_validity(self):
+        shifts = 0  # The number of tile shifts
+        flat_grid = sum(self.grid, [])  # flatten the grid -> This is only necessary no make coding less painful
+        flat_grid.remove(0)  # Remove the 0 value
+
+        # Traverse the grid to search for misplaced tiles.
+        for e in flat_grid:
+            for n in range(flat_grid.index(e), e):
+                if e > flat_grid[n]:
+                    shifts += 1
+        if shifts % 2 == 0:
+            return True
+        else:
+            return False
+
+    ###
+    ###### Print a grid to display the puzzle in the terminal
+    #########
+    ######
+    ###
+
+    def print_ascii_grid(self):
+        print("+---" * 3 + "+")
+        print("|", self.grid[0][0], "|", self.grid[0][1], "|", self.grid[0][2], "|", )
+        print("+---" * 3 + "+")
+        print("|", self.grid[1][0], "|", self.grid[1][1], "|", self.grid[1][2], "|", )
+        print("+---" * 3 + "+")
+        print("|", self.grid[2][0], "|", self.grid[2][1], "|", self.grid[2][2], "|", )
+        print("+---" * 3 + "+")
+
+    ###
+    ###### Print the grid in a very rudimentary matplotlib display
+    #########
+    ######
+    ###
+
+    def print_matplotlib_grid(self):
+        y = 1
+        fig, ax = plt.subplots()
+        ax.grid(which='major', color='black', linestyle='', linewidth=1)
+        ax.set_xlim([0, 3])
+        ax.set_ylim([0, 3])
+        for e in reversed(self.grid):
+            x = 1
+            n = 0
+            while n < 3:
+                ax.text(x - 0.5, y - 0.5, e[n], ha='center', va='center')
+                x += 1
+                n += 1
+            y += 1
+        plt.show()
--- Solution.py
+++ Solution.py
@@ -1,9 +1,13 @@
+import Grid
+
+
 class Solution:
     def __init__(self, grid):
         self.grid = grid.grid
 
     default_goal_state = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-    costs = {0: 0, 1: 9, 2: 9, 3: 9, 4: 9, 5: 9, 6: 9, 7: 9, 8: 9}
+    costs = {1: 9, 2: 9, 3: 9, 4: 9, 5: 9, 6: 9, 7: 9, 8: 9}
+    count = 0
 
     def get_tile(self, tile, search_grid):
         null_coords = []
@@ -14,18 +18,26 @@ class Solution:
                 return null_coords
 
     def solve_puzzle(self, grid):
+        if self.count == 20:
+            return
         if self.grid == self.default_goal_state:
             return True
+        self.costs = dict.fromkeys(self.costs, 9)
         self.calculate_cost()
         fringe = {key: value for (key, value) in self.costs.items() if value > 0}
-        to_move = min(fringe, key = fringe.get)
-        print("x")
-
+        to_move = min(fringe, key=fringe.get)
+        to_move_coords = self.get_tile(to_move, self.grid)
+        null_position = self.get_tile(0, self.grid)
+        self.grid[null_position[0]][null_position[1]] = to_move
+        self.grid[to_move_coords[0]][to_move_coords[1]] = 0
+        grid.print_ascii_grid()
+        self.count += 1
+        self.solve_puzzle(grid)
 
     def calculate_cost(self):
         null_position = self.get_tile(0, self.grid)
         if null_position[0] == 0:
-            if null_position[1] == 0:  # Calculate costs from 0 position than add 1
+            if null_position[1] == 0:
                 to_move_coords = [[0, 1], [1, 0]]
                 self.calculate_distance(to_move_coords, null_position)
             elif null_position[1] == 1:
@@ -58,22 +70,9 @@ class Solution:
     def calculate_distance(self, to_move_coords, null_position):
         for coords in to_move_coords:
             to_move = self.grid[coords[0]][coords[1]]
-            target_position = self.get_tile(to_move, self.default_goal_state)
-            self.costs[to_move] = abs(null_position[1] - target_position[1]) + abs(
-                null_position[0] - target_position[0]) + 1
-
-    def right(self, row, col):
-        self.grid[row][col + 1] = self.grid[row][col]
-        self.grid[row][col] = 0
-
-    def left(self, row, col):
-        self.grid[row][col - 1] = self.grid[row][col]
-        self.grid[row][col] = 0
-
-    def up(self, row, col):
-        self.grid[row - 1][col] = self.grid[row][col]
-        self.grid[row][col] = 0
-
-    def down(self, row, col):
-        self.grid[row + 1][col] = self.grid[row][col]
-        self.grid[row][col] = 0
+            if self.get_tile(to_move, self.grid) != self.get_tile(to_move, self.default_goal_state):
+                target_position = self.get_tile(to_move, self.default_goal_state)
+                self.costs[to_move] = abs(null_position[1] - target_position[1]) + abs(
+                    null_position[0] - target_position[0]) + 1
+            else:
+                self.costs[to_move] = 0
